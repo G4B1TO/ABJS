c60fd442c8d4938f8bdd76447605012239e47064
G4B1TO <jgabrielsr25@gmail.com>
2024-08-31 21:07:45 -0500

    Add files via upload

---
+++ metro.config.js
@@ -0,0 +1,18 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+    transformer: {
+    
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+    },
+    resolver: {
+     
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+    },
+  };
+  
